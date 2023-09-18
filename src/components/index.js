d504ce124cf919cdeb8f3ada89497334498b815b
unknown <verribeiro@hotmail.com>
2023-09-17 22:49:02 -0300

    Centralizando exports dos componentes em um arquivo unico

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import OrderByName from './OrderByName';
+import CharacterThumbnail from './CharacterThumbnail';
+import SearchInput from './SearchInput';
+import FavoriteButton from './FavoriteButton';
+
+export {
+  FavoriteButton,
+  OrderByName,
+  CharacterThumbnail,
+  SearchInput,
+};
